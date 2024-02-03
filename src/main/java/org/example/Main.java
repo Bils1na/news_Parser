dadb7542fea1907fea95cc96de1eea00263cf152
bilsina <bils1n@icloud.com>
2024-02-03 20:46:06 +0600

    0.01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,11 @@
 package org.example;
 
+import org.jsoup.nodes.Element;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(Parser.getBody());
-
+        Parser.printNews();
     }
 }
--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -3,14 +3,17 @@ package org.example;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
 
 
 public class Parser {
+
+    private static String url = "https://russian.rt.com/business";
+
     private static Document getPage() throws IOException {
-        String url = "https://russian.rt.com/business";
         Document page = Jsoup.parse(new URL(url), 3000);
         return page;
     }
@@ -18,6 +21,27 @@ public class Parser {
     public static Element getBody() throws IOException {
         Element body = getPage().selectFirst("body");
         return body;
+    }
+
+    public static Elements getNewsList() throws IOException {
+        Elements newsList = getBody().select("ul.listing__rows.listing__rows_short-news > li");
+        return newsList;
+    }
+
+    public static void printNews() throws IOException {
+        Elements newsList = getNewsList();
+
+        for (Element newsEl : newsList) {
+            Elements time = newsEl.select("time.date");
+            if (time.size() != 0) {
+                System.out.println(newsEl.select("time.date").text());
+                System.out.println(newsEl.select("a.link.link_color").text());
+                System.out.println(url
+                        + newsEl.select("a.link.link_color").attr("href")
+                        .replaceFirst("business/", ""));
+                System.out.println("-------------------------------------------------------------------------------------");
+            }
+        }
     }
 
 
